713dfa247e2b4f166b06425c4de05a7fb135d5d9
CoreyD97 <corey-dean06@hotmail.com>
2019-08-13 12:56:38 +0100

    Increment version number

--- src/main/java/com/coreyd97/stepper/Globals.java
+++ src/main/java/com/coreyd97/stepper/Globals.java
@@ -2,7 +2,7 @@ package com.coreyd97.stepper;
 
 public class Globals {
     public static final String EXTENSION_NAME = "Stepper";
-    public static final double VERSION = 1.1;
+    public static final double VERSION = 1.2;
     public static final String PREF_STEP_SEQUENCES = "sequences";
     public static final String PREF_PREV_VERSION = "previousVersion";
     public static final String PREF_VARS_IN_ALL_TOOLS = "enableVarsInAll";
