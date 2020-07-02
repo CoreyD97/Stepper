b07352c2bc5fc2d3ea793eb18a9e8c0a4fba94bd
Corey-Dean Arthur <1339555+CoreyD97@users.noreply.github.com>
2020-07-02 12:10:00 +0100

    Increment version to 1.4

--- src/main/java/com/coreyd97/stepper/Globals.java
+++ src/main/java/com/coreyd97/stepper/Globals.java
@@ -2,7 +2,7 @@ package com.coreyd97.stepper;
 
 public class Globals {
     public static final String EXTENSION_NAME = "Stepper";
-    public static final double VERSION = 1.3;
+    public static final double VERSION = 1.4;
     public static final String PREF_STEP_SEQUENCES = "sequences";
     public static final String PREF_PREV_VERSION = "previousVersion";
     public static final String PREF_VARS_IN_ALL_TOOLS = "enableVarsInAll";
